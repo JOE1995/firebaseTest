f8025181f7aef6d19f270e7181d07e900e6b125d
矢吹祐真 <yumaadobe@gmail.com>
2016-08-30 22:13:12 +0900

    initial comment

--- firebaseTest/ViewController.swift
+++ firebaseTest/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
 
+    var databaseRef:FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.blackColor().CGColor
+        
+        // Delegateを設定する.
+        nameTextField.delegate = self
+        messageTextField.delegate = self
+        
+        
+        databaseRef = FIRDatabase.database().reference()
+        databaseRef.observeEventType(.ChildAdded, withBlock: { snapshot in
+            if let name = snapshot.value!.objectForKey("name") as? String,
+                message = snapshot.value!.objectForKey("message") as? String {
+                self.textView.text = "\(self.textView.text)\n\(name) : \(message)"
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        
+        let messageData = ["name": nameTextField.text!, "message": messageTextField.text!]
+        databaseRef.childByAutoId().setValue(messageData)
+        
+        textField.resignFirstResponder()
+        messageTextField.text = ""
+        print("done")
+        
+        return true
+    }
 
 }
 
